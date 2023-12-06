4851cdcdeb7168cae29f271fc48bc99900fb8885
TurdubekovAli <153112692+TurdubekovAli@users.noreply.github.com>
2023-12-07 03:14:03 +0600

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.remotecontroller10
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.remotecontroller10.ui.theme.RemoteController10Theme
+import com.example.rm_10.BaseActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RemoteController10Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+
+        startActivity(Intent(this, BaseActivity::class.java))
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RemoteController10Theme {
+        Greeting("Android")
+    }
+}
