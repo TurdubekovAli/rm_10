ddd4f98fb678e85d3db4fa79d24ab453edd6d3bb
TurdubekovAli <153112692+TurdubekovAli@users.noreply.github.com>
2023-12-16 20:01:56 +0600

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+
+    }
+}
+
+rootProject.name = "Remote Controller 1.0"
+include(":app")
+include(":rm_10")
